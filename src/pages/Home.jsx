2367d3d354ded261e9bda62314fce8c0c670bad0
Jannatul <limajannat1675@gmail.com>
2025-04-22 07:56:58 +0600

    added Hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from './../components/Hero/Hero';
+import PhoneContainer from './../components/PhoneContainer/PhoneContainer';
 
 const Home = () => {
     return (
         <div>
-            
+            <Hero></Hero>
+            <PhoneContainer/>
         </div>
     );
 };
